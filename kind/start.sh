5da22d919d1680eae72e6c6a6552ff410f2b7a0b
Paulo Rogério Gomes e Silva <psilva.gomes.rogerio@gmail.com>
2025-01-23 21:26:52 -0300

    Correcoes

--- kind/start.sh
+++ kind/start.sh
@@ -21,7 +21,7 @@ colima_running=$(jq -r .status <<< $(colima list -j))
 
 [[ ${colima_running} == "Stopped" ]] && colima start --network-address --cpu 2 --memory 2 --disk 40
 
-colima ssh -- bash -c "sudo -i cat >> /tmp/sysctl.conf <<EOF
+colima ssh -- bash -c "sudo -h 127.0.0.1 -i cat >> /tmp/sysctl.conf <<EOF
 fs.inotify.max_user_watches = 1048576
 fs.inotify.max_user_instances = 512
 EOF"
