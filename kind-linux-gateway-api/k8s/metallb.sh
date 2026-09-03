3b51d23578ed575564f1fcc33f0a7425c71d1bde
Paulo Rogerio Gomes e Silva <prgomessilva@tjgo.jus.br>
2026-09-03 10:45:48 -0300

    Regex docker network inspec

--- kind-linux-gateway-api/k8s/metallb.sh
+++ kind-linux-gateway-api/k8s/metallb.sh
@@ -6,7 +6,7 @@ function output(){
   echo $(date +%Y-%m-%d-%H:%M:%S) - $@
 }
 
-export kind_cidr=$(docker network inspect -f '{{.IPAM.Config}}' kind | egrep -o '[0-9]{3}.[0-9]{2}.[0-9].[0-9]/[0-9]{2}')
+export kind_cidr=$(docker network inspect -f '{{.IPAM.Config}}' kind | egrep -o '[0-9]{1,}.[0-9]{1,}.[0-9].[0-9]/[0-9]{1,}')
 output "Kind CIDR..........: ${kind_cidr}"
 
 export kind_cidr_short=$(cut -d '.' -f1-3 <<< ${kind_cidr})
